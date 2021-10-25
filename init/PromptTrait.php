3a1395156c472aedd9883c4170a459fb031b802d
Дмитрий Мичкире <michkire@gmail.com>
2021-10-25 17:40:11 +0300

    reorganize

--- init/PromptTrait.php
+++ init/PromptTrait.php
@@ -20,19 +20,18 @@ trait PromptTrait
     public function user_prompt()
     {
         $data['author'] = $this->prompt('Author', getenv('DEVELOPER_NAME'));
-        $data['email'] = mb_strtolower($this->prompt('Email', getenv('DEVELOPER_EMAIL')));
+        $data['email'] = mb_strtolower($this->prompt('Email', getenv('DEVELOPER_EMAIL'), '|^[a-zA-Z0-9\s\-@]+$|'));
         $data['vendor'] = mb_strtolower($this->prompt('Vendor', getenv('VENDOR_NAME')));
-        $data['package'] = mb_strtolower($this->prompt('Package', ''));
-        var_dump($data);
-        die();
+        $data['package'] = mb_strtolower($this->prompt('Package', (basename(__DIR__))));
+        return $data;
     }
 
-    protected function prompt($message, $default, $pattern = "|[\w\s]+|")
+    protected function prompt($message, $default, $pattern = "|^[a-zA-Z0-9\s\-]+$|")
     {
         $default = $default ?: '';
         $input = $this->cli->input($message . ($default !== '' ? '[' . $default . ']' : '') . ':');
         $input->defaultTo($default);
-        $input->accept(function ($response) use ($pattern){
+        $input->accept(function ($response) use ($pattern) {
             return preg_match($pattern, trim($response));
         });
         return $input->prompt();

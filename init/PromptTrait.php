620b5fef7fae8add6b267420a72388ae630e0e96
Дмитрий Мичкире <michkire@gmail.com>
2021-10-25 17:24:17 +0300

    reorganize

--- init/PromptTrait.php
+++ init/PromptTrait.php
@@ -19,35 +19,12 @@ trait PromptTrait
      */
     public function user_prompt()
     {
-        $author = $this->prompt('Author', getenv('DEVELOPER_NAME'));
-        $email = $this->prompt('Email', getenv('DEVELOPER_EMAIL'));
-        var_dump($email);
+        $data['author'] = $this->prompt('Author', getenv('DEVELOPER_NAME'));
+        $data['email'] = mb_strtolower($this->prompt('Email', getenv('DEVELOPER_EMAIL')));
+        $data['vendor'] = mb_strtolower($this->prompt('Vendor', getenv('VENDOR_NAME')));
+        $data['package'] = mb_strtolower($this->prompt('Package', ''));
+        var_dump($data);
         die();
-
-        // Get developer email
-        $default = getenv('DEVELOPER_EMAIL') ?: '';
-        do {
-            $prompt = 'Email' . ($default !== '' ? '[' . $default . ']' : '') . ':';
-            $email = trim($this->cli->input($prompt)->prompt()) ?: $default;
-        } while (!trim($email));
-
-        // Try to get developer email from env
-        $env_email = getenv('DEVELOPER_EMAIL');
-        do {
-            if ($env_email) {
-                $email = mb_strtolower(trim($this->cli->input('Email[' . $env_email . ']:')->prompt()) ?: $env_email);
-            } else {
-                $email = mb_strtolower(trim($this->cli->input('Email:')->prompt()));
-            }
-        } while (!trim($email));
-
-        do {
-            $vendor = mb_strtolower(trim($this->cli->input('Vendor:')->prompt()) ?: 'peresmishnyk test');
-        } while (!trim($vendor));
-
-        do {
-            $package = mb_strtolower(trim($this->cli->input('Package:')->prompt()) ?: 'boilerplate package');
-        } while (!trim($package));
     }
 
     protected function prompt($message, $default)

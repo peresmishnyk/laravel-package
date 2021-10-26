4c66af950f77325fdadd8af3d336ae2ec6f26789
Дмитрий Мичкире <michkire@gmail.com>
2021-10-26 11:59:10 +0300

    Revert "reorganize"
    This reverts commit 941be154dc3c2969ef5cb14d1f3b2d6ddee82b65.

--- init/Init.php
+++ init/Init.php
@@ -9,18 +9,11 @@ use League\CLImate\CLImate;
  */
 class Init
 {
+    use ReplaceInFilesTrait;
     use RenameFilesTrait;
 
     protected $project_root;
     protected $cli;
-
-    protected $fileForReplace = [
-        './composer.json.template',
-        './tests/_laravel/composer.json',
-        './src/Boilerplate.php',
-        './src/BoilerplateFacade.php',
-        './src/BoilerplateServiceProvider.php',
-    ];
 
     /**
      *
@@ -30,7 +23,7 @@ class Init
         $this->setProjectRoot(realpath(__DIR__ . DIRECTORY_SEPARATOR . '..'));
         $data = $this->userPrompt();
         $this->replaceInFiles($data);
-//        $this->renameFiles();
+        $this->renameFiles();
 //        $this->cleanup();
     }
 
@@ -63,26 +56,7 @@ class Init
     }
 
 
-    protected function replaceInFiles($data)
-    {
-        foreach ($data as $key => $val) {
-            foreach (['kebab', 'snake', 'studly', 'camel'] as $filter) {
-                $replaces['{' . $key . '|' . $filter . '}'] = \Illuminate\Support\Str::$filter($val);
-            }
-            $replaces['{' . $key . '}'] = $val;
-        }
 
-        foreach ($this->filesForReplace as $file) {
-            replaceInFile($file, $replaces);
-        }
-    }
-
-    protected function replaceInFile($path, $replaces)
-    {
-        // Normalize path
-        $path = preg_replace('/^\.\//', $this->getProjectRoot(), str_replace('/', DIRECTORY_SEPARATOR, $path));
-        file_put_contents($path, str_replace(array_keys($replaces), array_values($replaces), file_get_contents($path)));
-    }
 
 
 

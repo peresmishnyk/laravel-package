6aad99cd51a1a8ba6a8a237912a8465ddb9c2c69
Дмитрий Мичкире <michkire@gmail.com>
2021-10-26 16:38:58 +0300

    reorganize

--- init/LaravelPrepareTrait.php
+++ init/LaravelPrepareTrait.php
@@ -12,5 +12,6 @@ trait LaravelPrepareTrait
 
         $root_dir = $this->getProjectRoot();
         file_exists($root_dir . '/tests/_laravel/.env') || copy($root_dir . '/tests/_laravel/.env.example', $root_dir . '/tests/_laravel/.env');
+        shell_exec("touch {$root_dir}/tests/_laravel/database/database.sqlite");
     }
 }
